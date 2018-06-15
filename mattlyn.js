f7d1926270b8e690bcc106881ccddf5a105a59ae
gcanco <gcaniglia1@gmail.com>
2018-06-15 18:03:32 -0500

    Update mattlyn.js

--- mattlyn.js
+++ mattlyn.js
@@ -9,10 +9,11 @@ var intro = new TypeIt('#intro', {
 .delete()
 .pause(100)
 .type('Mattlyn Cordova')
-
+/*
 var bio = new TypeIt('#bio', {
   speed: 25,
   cursor: false
 })
 .pause(5500)
 .type('Northwestern undergraduate studying Gender and Sexuality, Film and Media, and Latinx Studies')
+*/
